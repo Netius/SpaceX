d813aeb39f31a001ed22ac248feea75436a8e143
Fabio Neto <fabiomoraisnetosp@hotmail.com>
2021-02-16 19:06:10 +0100

    Upcoming launch comes from api

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
     const corsEnableUrl = "https://noroffcors.herokuapp.com/";
     const spacexUrl = corsEnableUrl + "https://api.spacexdata.com/v4/launches";
     const spacexLaunchpads = corsEnableUrl + "https://api.spacexdata.com/v4/launchpads";
+    const rocketsSpacex = corsEnableUrl + "https://api.spacexdata.com/v4/rockets";
 
     const calenderLaunches = document.getElementById("calendar-launches");
     const apiLoader = document.querySelector(".loader-container");
+
+    const upcomingLaunch = document.getElementById("upcoming-container");
+
     async function fetchSpacex() {
         try {
             apiLoader.style.display = "flex";
@@ -11,11 +15,15 @@
             const responseLaunches = await fetch(spacexUrl);
             const launches = await responseLaunches.json();
            
-            // Api call for Rockets
+            // Api call for Launchpads
             const responseLaunchpads = await fetch(spacexLaunchpads);
             const launchpads = await responseLaunchpads.json();
-            
-            createSpacexLaunches(launches ,launchpads);
+
+            // Api call for rockets
+            const responseRockets = await fetch(rocketsSpacex);
+            const rockets = await responseRockets.json();
+
+            createSpacexLaunches(launches ,launchpads, rockets);
             apiLoader.style.display = "none";
 
         } catch (error) {
@@ -26,17 +34,43 @@
     fetchSpacex();
 
 
-    function createSpacexLaunches(spacex , launchpads) { 
+    function createSpacexLaunches(spacex , launchpads, rockets) { 
         sortDateLaunches(spacex);
         let detailHtml = "";
+        let upcomingHtml = "";
         let counter = 0;
-        
+
         for(let launch of spacex){
             if(new Date(launch.date_local) > new Date() && (counter <= 2)){
                 const launchpad = launchpads.filter(element => element.id === launch.launchpad);
+                const rocket = rockets.filter(element => element.id === launch.rocket);
+
+                console.log(rocket);
+                console.log(rocket[0].flickr_images[0]);
                 
                 let formatedDate = formatDate(launch.date_local);
-               
+                
+                // Show upcoming launch i header
+                if (counter === 0){
+                    upcomingHtml = `
+                    <div class="main-hero-image">
+                        <div class="container-hero">
+                            <div class="hero-text">
+                                <h1 class="hero-header">Upcoming launch</h1>
+                                <h2 class="hero-starship">${launch.name}</h2>
+                                <p class="hero-date">${formatedDate}</p>
+                                <div>
+                                    <a class="btn-standard" title="Read more about upcoming launch" href="upcoming.html?${launch.id}">
+                                        Read more
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                    upcomingLaunch.innerHTML = upcomingHtml; 
+                }
+
                 detailHtml += `
                 <div class="launches-container">
                     <ul>
